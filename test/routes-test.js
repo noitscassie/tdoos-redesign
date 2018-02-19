9aeb4cd4baef4296187c8c13de244c8a9816a92d
Peter Johnstone <peter.wdj@gmail.com>
2018-02-19 12:05:16 +0000

    Creates first test file

---
+++ test/routes-test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app/server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Tests routes', () => {
+  describe('Tests index', () => {
+    it('Should return status code 200', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+});
