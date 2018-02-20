18ffe42c8e2d788db3f0837fcfd33d81dc8b94db
Peter Johnstone <peter.wdj@gmail.com>
2018-02-20 11:28:24 +0000

    Adds test for Scraper#getDefinition; renames fakeEntry to fakeTextEntry

--- test/units/scraper-test.js
+++ test/units/scraper-test.js
@@ -4,14 +4,20 @@ const helpers = require('./../helpers');
 const chai = require('chai');
 const Scraper = require('./../../app/scraper');
 
-const fakeEntry = helpers.fakeEntry;
+const fakeTextEntry = helpers.fakeTextEntry;
 const expect = chai.expect;
 
 describe('Scraper', () => {
   describe('getWord', () => {
-    it('returns a word from TDOOS', () => {
+    it('returns a word from a TDOOS entry', () => {
       const scraper = new Scraper();
-      expect(scraper.getWord(fakeEntry)).to.equal('kuebiko');
+      expect(scraper.getWord(fakeTextEntry)).to.equal('kuebiko');
+    });
+  });
+  describe('getDefinition', () => {
+    it('returns a definition from a TDOOS entry', () => {
+      const scraper = new Scraper();
+      expect(scraper.getDefinition(fakeTextEntry)).to.equal('n. some definition');
     });
   });
 });
