2c90cd0d057efaa2f4a4b1042ea51638137cf94a
Peter Johnstone <peter.wdj@gmail.com>
2018-02-20 15:21:26 +0000

    Refactors tests using before hook

--- test/units/scraper-test.js
+++ test/units/scraper-test.js
@@ -7,22 +7,25 @@ const Scraper = require('./../../app/lib/scraper');
 const fakeTextEntry = helpers.fakeTextEntry;
 const expect = chai.expect;
 
+
 describe('Scraper', () => {
+  let scraper;
+  before(() => {
+    scraper = new Scraper();
+  });
+
   describe('getWord', () => {
     it('returns a word from a TDOOS entry', () => {
-      const scraper = new Scraper();
       expect(scraper.getWord(fakeTextEntry)).to.eql('kuebiko');
     });
   });
   describe('getDefinition', () => {
     it('returns a definition from a TDOOS entry', () => {
-      const scraper = new Scraper();
       expect(scraper.getDefinition(fakeTextEntry)).to.eql('n. some definition');
     });
   });
   describe('getEntry', () => {
     it('returns a hash with a word and definition', () => {
-      const scraper = new Scraper();
       expect(scraper.getEntry(fakeTextEntry)).to.eql({
         word: 'kuebiko',
         definition: 'n. some definition'
