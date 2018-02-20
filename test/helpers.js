f8697b34f8af9a880d295065a8435af9f8daf56d
Peter Johnstone <peter.wdj@gmail.com>
2018-02-20 16:33:33 +0000

    Creates fakePage DOM object

--- test/helpers.js
+++ test/helpers.js
@@ -3,33 +3,21 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-const fakeTextEntry = new JSDOM('<div class="post text"><a herf="#">Some text</a><h2 class="title"><a href="#">kuebiko</a></h2><div class="content"><p><i>n</i>. some definition</p></div></div>');
+const fakeTextEntry = new JSDOM(`
+  <div class="post text">
+    <a herf="#">Some text</a>
+    <h2 class="title"><a href="#">kuebiko</a></h2>
+    <div class="content"><p><i>n</i>. some definition</p></div>
+  </div>
+  `);
+
+const fakePage = new JSDOM(`
+  <body>
+    <div class="post text"></div>
+    <div class="post video"></div>
+    <div class="post text"></div>
+  </body>
+  `);
 
 module.exports.fakeTextEntry = fakeTextEntry;
-// global.window = document.defaultView;
-
-// <div class="post text">
-// 	<a href="http://www.dictionaryofobscuresorrows.com/post/147440417070/kuebiko" class="icon"></a>
-// 	<h2 class="title"><a href="http://www.dictionaryofobscuresorrows.com/post/147440417070/kuebiko">kuebiko</a></h2>
-// 	<div class="content">
-// 		<p><i>n</i>. a state of exhaustion inspired by an act of senseless violence, which forces you to revise your image of what can happen in this world—mending the fences of your expectations, weeding out invasive truths, cultivating the perennial good that’s buried under the surface—before propping yourself up in the middle of it like an old scarecrow, who’s bursting at the seams but powerless to do anything but stand there and watch.</p>
-// 	</div>
-// 	<div class="meta">
-// 		<ul class="metaList">
-// 			<li class="posted">
-//         <a href="http://www.dictionaryofobscuresorrows.com/post/147440417070/kuebiko"><span>Posted 1 year ago</span></a>
-//       </li>
-// 			<li class="author">
-//         by <a href="http://jkcreative.tumblr.com/">jkcreative</a>
-//       </li>
-// 			<li class="notes">
-//         <a href="http://www.dictionaryofobscuresorrows.com/post/147440417070/kuebiko#notes"><span>2,223 notes</span></a>
-//       </li>
-//       <!-- <li class="comments">
-//         <a href="http://www.dictionaryofobscuresorrows.com/post/147440417070/kuebiko#disqus_thread"><span class="dsq-comment-count">Comments</span></a>
-//       </li> -->
-// 		</ul>
-// 		<div class="clear">
-//     </div>
-// 	</div>
-// </div>
+module.exports.fakePage = fakePage;
