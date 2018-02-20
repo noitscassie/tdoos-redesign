9db0d76e79d32ef372f1751b617051d9bcf5a23b
Peter Johnstone <peter.wdj@gmail.com>
2018-02-20 11:29:27 +0000

    Adds #getDefinition function

--- app/scraper.js
+++ app/scraper.js
@@ -4,6 +4,10 @@ class Scraper {
   getWord(entry) {
     return entry.window.document.querySelector('.title').textContent;
   }
+
+  getDefinition(entry) {
+    return entry.window.document.querySelector('.content').textContent;
+  }
 }
 
 module.exports = Scraper;
