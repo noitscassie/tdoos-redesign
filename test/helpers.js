c7798e098d19762ba724006bd48490e6f9812f5c
Peter Johnstone <peter.wdj@gmail.com>
2018-02-21 09:06:20 +0000

    Extends fakePage element top include content within divs

--- test/helpers.js
+++ test/helpers.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom;
 
 const fakeTextEntry = new JSDOM(`
   <div class="post text">
-    <a herf="#">Some text</a>
+    <a href="#">Some text</a>
     <h2 class="title"><a href="#">kuebiko</a></h2>
     <div class="content"><p><i>n</i>. some definition</p></div>
   </div>
@@ -13,9 +13,21 @@ const fakeTextEntry = new JSDOM(`
 
 const fakePage = new JSDOM(`
   <body>
-    <div class="post text"></div>
-    <div class="post video"></div>
-    <div class="post text"></div>
+    <div class="post text">
+      <a href="#">Some text</a>
+      <h2 class="title"><a href="#">kuebiko</a></h2>
+      <div class="content"><p><i>n</i>. some definition</p></div>
+    </div>
+    <div class="post video">
+      <a href="#">Some text</a>
+      <h2 class="title"><a href="#">lutalica</a></h2>
+      <div class="content"><p><i>n</i>. another definition</p></div>
+    </div>
+    <div class="post text">
+      <a href="#">Some text</a>
+      <h2 class="title"><a href="#">lachesism</a></h2>
+      <div class="content"><p><i>n</i>. a third definition definition</p></div>
+    </div>
   </body>
   `);
 
