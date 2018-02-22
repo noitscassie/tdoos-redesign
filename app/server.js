4bd5043dfbd9a86a500c5048512fedbaf2122d7a
Peter Johnstone <peter.wdj@gmail.com>
2018-02-22 11:31:46 +0000

    Sends array of entries in response body at /GET /

--- app/server.js
+++ app/server.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const express = require('express');
+const request = require('request');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const Scraper = require('./lib/scraper');
 
 const app = express();
+const scraper = new Scraper();
+const url = 'http://www.dictionaryofobscuresorrows.com/page/2';
 
 app.get('/', (req, res) => {
-  res.send(200);
+  let entries;
+  request(url, function(err, response, html) {
+    const page = new JSDOM(html);
+    entries = scraper.getPageEntries(page);
+    res.send(entries);
+  });
 });
+
+app.listen(8000);
 
 module.exports = app;
