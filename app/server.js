e06729a95b91175e2b9522e26f19ccdaa5c9bc0f
Peter Johnstone <peter.wdj@gmail.com>
2018-03-14 11:55:59 +0000

    Adds Node environments to configure databases

--- app/server.js
+++ app/server.js
@@ -1,14 +1,18 @@
 'use strict';
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const routes = require('./routes');
+const config = require('./config').getEnv(process.env.NODE_ENV);
 
 const app = express();
-const url = 'mongodb://127.0.0.1:27017/obscure_sorrows_dev';
+const dbURL = `mongodb://127.0.0.1:27017/${config.database}`;
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
+MongoClient.connect(dbURL, function(err, db) {
+  const database = db.db(config.database);
+  database.createCollection('entries');
   routes(app);
 })
 
