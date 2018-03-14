d866cd78f9858984645b9c3cb82fba174cdf5d98
Peter Johnstone <peter.wdj@gmail.com>
2018-03-14 11:07:32 +0000

    Extracts GET / route to routes.js

--- app/server.js
+++ app/server.js
@@ -1,23 +1,11 @@
 'use strict';
 
 const express = require('express');
-const request = require('request');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const Scraper = require('./lib/scraper');
 
+const routes = require('./routes');
 const app = express();
-const scraper = new Scraper();
-const url = 'http://www.dictionaryofobscuresorrows.com/page/2';
 
-app.get('/', (req, res) => {
-  let entries;
-  request(url, function(err, response, html) {
-    const page = new JSDOM(html);
-    entries = scraper.getPageEntries(page);
-    res.send(entries);
-  });
-});
+routes(app);
 
 app.listen(8000);
 
