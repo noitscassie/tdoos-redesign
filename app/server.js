0ef53df55ad52f07f79460efcde51360ec5cf95e
Peter Johnstone <peter.wdj@gmail.com>
2018-03-14 11:21:14 +0000

    Adds mongodb and calls routes file within a mongodb connection

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const routes = require('./routes');
-const app = express();
 
-routes(app);
+const app = express();
+const url = 'mongodb://127.0.0.1:27017/obscure_sorrows_dev';
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  routes(app);
+})
 
 app.listen(8000);
 
