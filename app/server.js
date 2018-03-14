c504e645f2e21fe2668b67a715559ae7b22de345
Peter Johnstone <peter.wdj@gmail.com>
2018-03-14 12:52:16 +0000

    Uses mongoose ORM instead of MongoDB directly

--- app/server.js
+++ app/server.js
@@ -3,18 +3,20 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const config = require('./config').getEnv(process.env.NODE_ENV);
 
 const app = express();
 const dbURL = `mongodb://127.0.0.1:27017/${config.database}`;
 
-MongoClient.connect(dbURL, function(err, db) {
-  const database = db.db(config.database);
-  database.createCollection('entries');
+mongoose.connect(dbURL);
+const db = mongoose.connection;
+
+db.once('open', function() {
+  db.createCollection('entries');
   routes(app);
-})
+});
 
 app.listen(8000);
 
